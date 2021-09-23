a5241ff63cc282d76ee7d6da3e6b779651f71c36
Angelus123 <izerefaifelix@gmail.com>
2021-09-23 05:45:37 -0400

    Done with lists

--- lists.py
+++ lists.py
@@ -1,1 +1,29 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
+# create list
+numbers =[1,2,3,4,5]
+fruits = ['Apples', 'Orange', 'Grapes', ]
+#use a constructor
+# numbers2 =list((1,2,3,4,5))
+# print (numbers, numbers2)
+
+# Get a value 
+print (fruits[1])
+# Get length
+print(len(fruits))
+# Append to list
+fruits.append ('Mangos')
+# Remove from list
+fruits.remove('Grapes')
+# Insert into position
+fruits.insert(2, 'Strawberries ')
+# Change value
+fruits[0] = 'Blueberries'
+# Remove with pop
+fruits.pop(2)
+#Reverse list
+print(fruits)
+fruits.reverse()
+print(fruits)
+fruits.sort()
+print(fruits)
+
--- strings.py
+++ strings.py
@@ -1,6 +1,21 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
 
-
+name = 'Brad'
+age = 37
+# Concatenate
+# print('Hello, my name is'+name+ ' and I am '+str(age))
 # String Formatting
-
-# String Methods
+# Argument by position
+# print('My name is {name} and I am {age}'.format(name=name, age=age))
+# F-String (3.6+) 
+# print (f'Hello, my is {name} and I am {age}')
+s='hello world'
+# Capitalize string
+# print(s.capitalize())
+# print(len(s))
+# print(s.upper())
+# print(s.lower())
+# print(s.swapcase())
+print (s.replace('world', 'everyone'))
+print (s.isnumeric())
+print(s.isalnum())
